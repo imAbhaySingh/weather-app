5e277997221dfadc80d84b8575cead3569d9bb3b
imAbhaySingh <abhaybhadauriya2003@gmail.com>
2023-08-22 18:55:44 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2b1a224e41faa00975bde2e07b184dfc&units=metric`;
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("")
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Your Location"
+          type="text"
+        />
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            <h1>{data.main?.temp}°C</h1>
+          </div>
+          <div className="desciption">
+            <p>{data.weather?.[0]?.main}</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">{data.main?.feels_like}°C</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">{data.main?.humidity}%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">{data.wind?.speed}KMH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
